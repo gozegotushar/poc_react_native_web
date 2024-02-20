9dce078b2b194963eaff06ef43bddb44160aa755
tusharpandey <tpandey@gozego.com>
2024-02-20 23:01:14 +0530

    refactor: adding gesture handler

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <App />
+    </GestureHandlerRootView>
   </React.StrictMode>
 );
 
