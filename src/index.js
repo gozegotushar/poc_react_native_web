9ff396184fae0fc67409852d909c591430afe58b
tusharpandey <tpandey@gozego.com>
2024-02-21 12:38:40 +0530

    refactor: working repo

--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <App />
-    </GestureHandlerRootView>
+    <App />
   </React.StrictMode>
 );
 
